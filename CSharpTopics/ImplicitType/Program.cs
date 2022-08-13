e000aa3f6fbf6b7a13f946f78615d8e2985ea90a
devskil <meet.dasnato@gmail.com>
2022-08-13 10:38:22 +0600

    ..

---
+++ CSharpTopics/ImplicitType/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Implicit Example");
+
+int x= 10;
+var y= 20.20;
+
+Console.WriteLine(" This is an implicit value : "+y);
